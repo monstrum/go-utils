9824f9adbeeb2568613f48d1720417d396019676
Erwin Eu <eu.erwin@xie-group.com>
2020-10-23 19:59:43 +0200

    Test

--- config.go
+++ config.go
@@ -7,8 +7,30 @@ import (
 	"os"
 )
 
-func Parse(file string) (map[string]interface{}, error) {
-	var result map[string]interface{}
+type MappedString map[string]interface{}
+
+func Parse(file string) interface{} {
+	parsed, err := ParseStruct(file)
+	if err != nil {
+		return nil
+	}
+	return parsed
+}
+
+func ParseMap(file string) (map[string]interface{}, error) {
+	parsed, err := ParseStruct(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if brands, ok := parsed.(MappedString); ok {
+		return brands, nil
+	}
+	return nil, errors.New("error")
+}
+
+func ParseStruct(file string) (interface{}, error) {
+	var result *interface{}
 	jsonFile, err := os.Open(file)
 	if err != nil {
 		return nil, errors.New("missing file")
